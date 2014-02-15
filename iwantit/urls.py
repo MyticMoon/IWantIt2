f6c1ce88cec355f044ded2c312a2ea84703de2e5
manhhiep92 <hiep1@e.ntu.edu.sg>
2014-02-16 05:35:47 +0800

    authentication

--- iwantit/urls.py
+++ iwantit/urls.py
@@ -7,9 +7,12 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^profile/', views.profile, name='profile'),
     url(r'^contact/', views.contact, name='contact'),
-    url(r'^searchitem/', views.searchitem, name='contact'),
-    url(r'^shareit/', views.shareItAction, name='contact'),
-    url(r'^additem/', views.addItemAction, name='contact'),
+    url(r'^searchitem/', views.searchitem, name='searchitem'),
+    url(r'^shareit/', views.shareItAction, name='shareit'),
+    url(r'^additem/', views.addItemAction, name='additem'),
     url(r'^vault/', views.vault, name='vault'),
-    url(r'^pickitem/', views.pickitem, name='contact')
+    url(r'^pickitem/', views.pickitem, name='contact'),
+    url(r'^loginView/', views.loginView, name='loginView'),
+    url(r'^login', views.loginAction, name='login'),
+    url(r'^logout/', views.logout, name='logout')
 )
--- iwantit/views.py
+++ iwantit/views.py
@@ -5,7 +5,7 @@ from django.db import connection, transaction
 # import models
 from django.template.loader import get_template
 from django.template import Context
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, redirect
 from random import randint
 #
 # def index(request):
@@ -14,38 +14,70 @@ from random import randint
 #     return render(request)
 from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
+def loginAction(request):
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    if username and password:
+        user_query = "SELECT id FROM userTable WHERE username='" + username + "' AND pw='" + password + "'"
+        user_cursor = connection.cursor()
+        user_cursor.execute(user_query)
+        user = user_cursor.fetchall()
+        if len(user):
+            request.session["user_id"] = user[0][0]
+            return redirect('/iwantit/')
+        else:
+            return render_to_response('iwantit/login.html', None)
+    else:
+        return render_to_response('iwantit/login.html', None)
+
+def loginView(request):
+    if not request.session.user_id:
+        return render_to_response('iwantit/login.html', None)
+
+@csrf_exempt
+def logout(request):
+    if request.session["user_id"] != None:
+        request.session["user_id"] = None
+        return redirect('/iwantit/login')
+    else:
+        return redirect('/iwantit/')
 
 @transaction.commit_on_success
 def index(request):
-    user_id = 0
-    prod_query = "select donatedList.*, userTable.name from donatedList left join userTable on donatedList.donateduserid = userTable.id where donatedList.donateduserid <> " + str(user_id) + " order by donatedate desc limit 7;"
-    cursor1 = connection.cursor()
-    cursor1.execute(prod_query)
-    prod_result = cursor1.fetchall()
-    user_query = "SELECT name, address, birthday, gender, email, hp, imageUrl, currentPoints, numDonatedItem, numRedeemedItem FROM userTable WHERE id = " + str(user_id)
-    user_cursor = connection.cursor()
-    user_cursor.execute(user_query)
-    user = user_cursor.fetchall()[0]
-    # template = loader.get_template('iwantit/index.html')
-    # context = RequestContext(request, prod_result)
-    # return HttpResponse(template.render(context))
-    return render_to_response('iwantit/index.html', {'prod_result': prod_result, 'user': user})
+    if 'user_id' in request.session and request.session["user_id"] != None:
+        prod_query = "select donatedList.*, userTable.name from donatedList left join userTable on donatedList.donateduserid = userTable.id where donatedList.donateduserid <> " + str(request.session["user_id"]) + " order by donatedate desc limit 7;"
+        cursor1 = connection.cursor()
+        cursor1.execute(prod_query)
+        prod_result = cursor1.fetchall()
+        user_query = "SELECT name, address, birthday, gender, email, hp, imageUrl, currentPoints, numDonatedItem, numRedeemedItem FROM userTable WHERE id = " + str(request.session["user_id"])
+        user_cursor = connection.cursor()
+        user_cursor.execute(user_query)
+        user = user_cursor.fetchall()[0]
+        # template = loader.get_template('iwantit/index.html')
+        # context = RequestContext(request, prod_result)
+        # return HttpResponse(template.render(context))
+        return render_to_response('iwantit/index.html', {'prod_result': prod_result, 'user': user})
+    else:
+        return render_to_response('iwantit/login.html', None)
 
 def profile(request):
-    user_id = 0
-    user_query = "SELECT name, address, birthday, gender, email, hp, imageUrl, currentPoints, numDonatedItem, numRedeemedItem FROM userTable WHERE id = " + str(user_id)
-    user_cursor = connection.cursor()
-    user_cursor.execute(user_query)
-    user = user_cursor.fetchall()[0]
-    donation_query = "SELECT donatedList.* FROM donatedList WHERE donatedUserId = " + str(user_id)
-    donation_cursor = connection.cursor()
-    donation_cursor.execute(donation_query)
-    donations = donation_cursor.fetchall()
-    redeem_query = "SELECT donatedList.*, transactionList.* FROM transactionList LEFT JOIN donatedList ON transactionList.itemid = donatedList.id WHERE transactionList.purchaseduserid = " + str(user_id)
-    redeem_cursor = connection.cursor()
-    redeem_cursor.execute(redeem_query)
-    redeems = redeem_cursor.fetchall()
-    return render_to_response('iwantit/profile.html', {'user': user, 'donations': donations, 'redeems': redeems})
+    if 'user_id' in request.session and request.session["user_id"] != None:
+        user_query = "SELECT name, address, birthday, gender, email, hp, imageUrl, currentPoints, numDonatedItem, numRedeemedItem FROM userTable WHERE id = " + str(request.session["user_id"])
+        user_cursor = connection.cursor()
+        user_cursor.execute(user_query)
+        user = user_cursor.fetchall()[0]
+        donation_query = "SELECT donatedList.* FROM donatedList WHERE donatedUserId = " + str(request.session["user_id"])
+        donation_cursor = connection.cursor()
+        donation_cursor.execute(donation_query)
+        donations = donation_cursor.fetchall()
+        redeem_query = "SELECT donatedList.*, transactionList.* FROM transactionList LEFT JOIN donatedList ON transactionList.itemid = donatedList.id WHERE transactionList.purchaseduserid = " + str(request.session["user_id"])
+        redeem_cursor = connection.cursor()
+        redeem_cursor.execute(redeem_query)
+        redeems = redeem_cursor.fetchall()
+        return render_to_response('iwantit/profile.html', {'user': user, 'donations': donations, 'redeems': redeems})
+    else:
+        return render_to_response('iwantit/login.html', None)
 
 def contact(request):
     template = loader.get_template('iwantit/contact.html')
@@ -53,16 +85,18 @@ def contact(request):
     return HttpResponse(template.render(context))
 
 def vault(request):
-    user_id = 0
-    item_query = "select donatedList.*, userTable.name from donatedList left join userTable on donatedList.donateduserid = userTable.id where donatedList.donateduserid <> " + str(user_id)
-    item_cursor = connection.cursor()
-    item_cursor.execute(item_query)
-    items = item_cursor.fetchall()
-    user_query = "SELECT name, address, birthday, gender, email, hp, imageUrl, currentPoints, numDonatedItem, numRedeemedItem FROM userTable WHERE id = " + str(user_id)
-    user_cursor = connection.cursor()
-    user_cursor.execute(user_query)
-    user = user_cursor.fetchall()[0]
-    return render_to_response('iwantit/vault.html', {'user': user, 'items': items})
+    if 'user_id' in request.session and request.session["user_id"] != None:
+        item_query = "select donatedList.*, userTable.name from donatedList left join userTable on donatedList.donateduserid = userTable.id where donatedList.donateduserid <> " + str(request.session["user_id"])
+        item_cursor = connection.cursor()
+        item_cursor.execute(item_query)
+        items = item_cursor.fetchall()
+        user_query = "SELECT name, address, birthday, gender, email, hp, imageUrl, currentPoints, numDonatedItem, numRedeemedItem FROM userTable WHERE id = " + str(request.session["user_id"])
+        user_cursor = connection.cursor()
+        user_cursor.execute(user_query)
+        user = user_cursor.fetchall()[0]
+        return render_to_response('iwantit/vault.html', {'user': user, 'items': items})
+    else:
+        return render_to_response('iwantit/login.html', None)
 
 @csrf_exempt
 def searchitem(request):
@@ -84,7 +118,7 @@ def shareItAction(request):
 def addItemAction(request):
     if request.method != "POST":
         return HttpResponse("Bad request")
-    userID = 0
+    userID = request.session["user_id"]
     categoryID = 1
     #TODO hard code to 0 here
     itemName = request.POST.get("name")
