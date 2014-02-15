1bc97d26638ef6c7396f08cf71f0b566371c09ce
Thanh Dat <vietnamknight2005@gmail.com>
2014-02-16 02:56:30 +0800

    fix template url

--- iwantit/views.py
+++ iwantit/views.py
@@ -2,7 +2,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.db import connection, transaction
-#import models
+# import models
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render, render_to_response
@@ -17,7 +17,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 @transaction.commit_on_success
 def index(request):
-    prod_query = "select * from donatedList"
+    user_id = 0
+    prod_query = "select donatedList.*, userTable.name from donatedList left join userTable on donatedList.donateduserid = userTable.id where donatedList.donateduserid <> " + str(user_id) + " order by donatedate desc limit 10;"
     cursor1 = connection.cursor()
     cursor1.execute(prod_query)
     prod_result = cursor1.fetchall()
@@ -32,11 +33,15 @@ def profile(request):
     user_cursor = connection.cursor()
     user_cursor.execute(user_query)
     user = user_cursor.fetchall()[0]
-    donation_query = "SELECT categoryID, itemName, description, imageUrl, equivalentPoints, status, donateDate FROM donatedList WHERE donatedUserId = " + str(user_id)
+    donation_query = "SELECT donatedList.* FROM donatedList WHERE donatedUserId = " + str(user_id)
     donation_cursor = connection.cursor()
     donation_cursor.execute(donation_query)
     donations = donation_cursor.fetchall()
-    return render_to_response('iwantit/profile.html', {'user': user, 'donations': donations})
+    redeem_query = "SELECT donatedList.*, transactionList.* FROM transactionList LEFT JOIN donatedList ON transactionList.itemid = donatedList.id WHERE transactionList.purchaseduserid = " + str(user_id)
+    redeem_cursor = connection.cursor()
+    redeem_cursor.execute(redeem_query)
+    redeems = redeem_cursor.fetchall()
+    return render_to_response('iwantit/profile.html', {'user': user, 'donations': donations, 'redeems': redeems})
 
 def contact(request):
     template = loader.get_template('iwantit/contact.html')
@@ -53,7 +58,7 @@ def searchitem(request):
     # template = loader.get_template('iwantit/index.html')
     # context = RequestContext(request, prod_result)
     # return HttpResponse(template.render(context))
-    return render_to_response('iwantit/index.html', {'prod_result': prod_result})
+    return render_to_response('iwantit/search.html', {'prod_result': prod_result})
 
 def shareItAction(request):
     return render_to_response('iwantit/shareit.html', {})
@@ -81,3 +86,4 @@ def addItemAction(request):
     transaction.commit()
 
     return render_to_response('iwantit/successadditem.html', {'point': str(point)})
+
